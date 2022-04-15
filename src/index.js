1e0460a64c738d8b8ede95d4cfcf64280c0e12a1
ForHemer <foreste.hemerson@gmail.com>
2022-04-14 23:41:22 -0400

    Design application using bootstrap and plain CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import elementStore from './redux/configureStore';
@@ -8,9 +7,7 @@ import elementStore from './redux/configureStore';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={elementStore}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
