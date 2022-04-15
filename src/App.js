e2cebba6687be67c4f298d731d6657ab7db96714
ForHemer <foreste.hemerson@gmail.com>
2022-04-14 20:16:52 -0400

    Update App.js file

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Zoo from './components/zoo/zoo';
+import AnimalDetail from './components/animal/details';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Zoo />} />
+        <Route path="/Zoo" element={<Zoo />} />
+        <Route path="/AnimalDetail" element={<AnimalDetail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
